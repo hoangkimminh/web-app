8ee2ecc9729056b5ca39d6fe04f3ce2fc6ae4ec8
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2020-02-25 21:37:20 +0700

    Fix side bar doesn't cover entire screen height, again

--- frontend/components/layouts/side-bar.js
+++ frontend/components/layouts/side-bar.js
@@ -133,7 +133,7 @@ const SideBar = () => {
       <Brand />
       <style jsx>{`
         section {
-          height: 100%;
+          min-height: 100vh;
         }
       `}</style>
     </section>
